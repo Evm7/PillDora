66f2a1e1aabc90de8821119198b080561ad42f76
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2019-10-17 21:07:28 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -116,11 +116,11 @@ def start(update, context):
     logger.info('User '+ name+' has connected to AideBot: ID is ' + str(user_id))
     context.bot.send_message(chat_id=user_id, text=("Welcome " + name + " ! My name is AideBot"))
 
-    if (user_verification(user_id)):
+    if (user_verification(user_id)=="True"):
         update.message.reply_text("Enter your password in order to get Assistance:")
         return set_state(user_id, LOGIN)
     else:
-        context.bot.send_message(chat_id=update.message.chat_id, text=("Welcome to the HealthCare Assistant AideBot."
+        context.bot.send_message(chat_id=update.message.chat_id, text=("Welcome to the HealthCare Assistant AideBot!\n"
                                                                        "Enter new password for creating your account:"))
     return set_state(user_id, NEW_USER)
 
@@ -143,7 +143,8 @@ def user_verification(user_id):
     set_function(user_id, 'CHECK USER')
     set_query(user_id, ["user_id"], [str(user_id)])
     query=create_query(user_id)
-    return (send_query(user_id, query))
+    response=send_query(user_id, query)
+    return (json.loads(response)["parameters"]["boolean"])
 
 
 # Verificates password for UserID in DataBase
@@ -151,14 +152,15 @@ def pwd_verification(password, user_id):
     set_function(user_id, 'CHECK PASSWORD')
     set_query(user_id, ['password'], [password])
     query = create_query(user_id)
-    return (send_query(user_id, query))
+    response=send_query(user_id, query)
+    return (json.loads(response)["parameters"]["boolean"])
 
 # function used to Introduce Password
 @run_async
 def intr_pwd(update, context):
     password = update.message.text
     logger.info('Password for user ' +get_name(update.message.from_user)+ ' is ' + password)
-    if (pwd_verification(password, update.message.from_user.id) == False):
+    if (pwd_verification(password, update.message.from_user.id) == "False"):
         update.message.reply_text("Wrong Password. Enter correct password again:")
         return set_state(update.message.from_user.id, LOGIN)
     update.message.reply_text('Welcome ' + get_name(update.message.from_user) + '. How can I help you?',
@@ -170,7 +172,7 @@ def intr_pwd(update, context):
 @run_async
 def new_user(update, context):
     password = update.message.text
-    logger.info('User introduce new password:  ' + password)
+    logger.info('User introduced new password:  ' + password)
     user_id= update.message.from_user.id
 
     # check for password difficulty.
@@ -192,7 +194,7 @@ def new_user(update, context):
         update.message.reply_text("Valid Password")
         # Introduce new UserID-Password to DataBase
         set_function(user_id, 'NEW PASSWORD')
-        set_query(['new_password'], [password])
+        set_query(user_id, ["new_password"], [password])
         query = create_query(user_id)
         send_query(user_id, query)
         update.message.reply_text('Welcome ' + get_name(update.message.from_user) + '. How can I help you?', reply_markup=markup)
@@ -208,11 +210,11 @@ def choose_function(update, context):
         query = create_query(user_id)
         response=json.loads(send_query(user_id, query))
         if(response['function']=='INTRODUCE MEDICINE'):
-            if(response['parameters']=="Code 0"):
+            if(response['parameters']["Code"]=="0"):
                 logger.info("Medicine correctly introduced")
-            elif (response['parameters'] == "Code 1"):
+            elif (response['parameters']["Code"]=="1"):
                 logger.info("Medicine already in the database with different frequencies. PROBLEM")
-            elif (response['parameters'] == "Code 2"):
+            elif (response['parameters']["Code"]=="2"):
                 logger.info("Medicine already in the database with same frequencies. NO PROBLEM")
         if (response['function'] == "DELETE REMINDER"):
             if (response['parameters']):
@@ -224,7 +226,7 @@ def choose_function(update, context):
                 return set_state(update.message.from_user.id, CHOOSING)
         if (response['function'] == "JOURNEY"):
             logger.info("Medicines to take during journey correctly retrieved")
-            update.message.reply_text("Medicines to take during journey:\n"+ response['parameters'])
+            update.message.reply_text("Medicines to take during journey:\n"+ response['parameters']["journey_info"])
 
     set_query(user_id, ["None"], ["None"])
     set_function(user_id, "None")
@@ -284,22 +286,22 @@ def inline_handler(update, context):
     if selected:
         if (get_states(user_id)[0] == CHOOSING):
             context.bot.send_message(chat_id=user_id,
-                         text="You selected %s" % (date.strftime("%d/%m/%Y")),
+                         text="You selected %s" % (date.strftime("%Y-%m-%d")),
                          reply_markup=ReplyKeyboardRemove())
         if(get_states(user_id)[0]==CHOOSING):
-            get_calendar_tasks(update, context, date.strftime("%d/%m/%Y"), user_id)
+            get_calendar_tasks(update, context, date.strftime("%Y-%m-%d"), user_id)
             set_state(user_id, CHOOSING)
         elif(get_states(user_id)[0]==JOURNEY):
-            set_journey(update, context, date.strftime("%d/%m/%Y"))
+            set_journey(update, context, date.strftime("%Y-%m-%d"))
             if(get_states(user_id)[1]==CHOOSING):
                  set_state(user_id, JOURNEY)
             elif(get_states(user_id)[1]==JOURNEY):
                  set_state(user_id, JOURNEY)
         elif(get_states(user_id)[0]==INTR_MEDICINE):
             context.bot.send_message(chat_id=user_id,
-                                     text=date.strftime("%d/%m/%Y"),
+                                     text=date.strftime("%Y-%m-%d"),
                                      reply_markup=ReplyKeyboardRemove())
-            set_medicine(user_id, get_counter(user_id), date.strftime("%d/%m/%Y"))
+            set_medicine(user_id, get_counter(user_id), date.strftime("%Y-%m-%d"))
             send_new_medicine(update,context)
 
 @run_async
@@ -311,7 +313,7 @@ def get_calendar_tasks(update, context, date, user_id):
     query = create_query(user_id)
     response= send_query(user_id, query)
     context.bot.send_message(chat_id=user_id,
-                             text="Reminders for "+ date+ " : "+response['parameters'])
+                             text="Reminders for "+ date+ " : "+response['parameters']['tasks'])
     context.bot.send_message(chat_id=user_id, text="Is there any other way I can help you?",
                              reply_markup=markup)
 
@@ -324,7 +326,7 @@ def see_history(update, context):
     set_query(user_id, ["user_id"], [str(user_id)])
     query = create_query(user_id)
     response=send_query(user_id, query)
-    update.message.reply_text("To som up, you are currently taking these meds:\n"+response['parameters'])
+    update.message.reply_text("To som up, you are currently taking these meds:\n"+response['parameters']['reminder_info'])
     set_query(user_id,["None"],["None"])
     return choose_function(update, context)
 
@@ -342,9 +344,13 @@ def get_medicine_CN(update, context):
     set_query(user_id, ["CN"],[medicine_CN])
     query = create_query(user_id)
     response = send_query(user_id, query)
-    update.message.reply_text('Reminder asked to be removed:\n'+ response['parameters'])
+    reminder_info=response['parameters']['reminder_info']
+    if(reminder_info=="False"):
+        if update.message.reply_text('CN introduced is wrong, there is not any med with this CN'):
+            return set_state(user_id, CHOOSING)
+    update.message.reply_text('Reminder asked to be removed:\n'+ reminder_info)
     update.message.reply_text('Is this the reminder you want to remove? ', reply_markup=yes_no_markup)
-    set_query(user_id, ["CN"], [response['parameters']['CN']])
+    set_query(user_id, ["CN"], [response['parameters']['reminder_info']])
     set_function(user_id, 'DELETE REMINDER')
     return set_state(user_id, CHECK_REM)
 
